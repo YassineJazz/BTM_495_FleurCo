85b5c9ff0ac8765c373e4349ad4cedddcfe4897f
OlivierPerron <oli.perron@hotmail.com>
2024-11-08 20:02:16 -0500

    menu update

--- app/Program.cs
+++ app/Program.cs
@@ -1,42 +1,106 @@
 ﻿class Program
 {
     static readonly Menu MenuOption = new(["1. Inventory", "2. Orders", "3. Sales Forecasts", "4. Exit"]);
-    static readonly Menu InventoryOption = new(["1. Display Inventory", "2. Add New Product", "3. Modify Existing Product", "4. Remove Product", "5. Exit"]);
-
+    static readonly Menu InventoryOption = new(["1. Display Inventory", "2. Add New Product", "3. Modify Existing Product", "4. Remove Product", "5. Go Back", "6. Exit"]);
+    static readonly Menu OrderOption = new(["1. Customer Orders", "2. Backorders", "3. Display Past Orders", "4. Go Back", "5. Exit"]);
+    static readonly Menu ForecastOption = new(["1. Create Forecast", "2. Display Past Forecasts", "3. Go Back", "4. Exit"]);
     static void Main(string[] args)
     {
         bool exit = false;
         while (!exit)
         {
             Console.Clear();
-            Console.WriteLine("FleurCo Inventory System");
+            Console.WriteLine("FleurCo Inventory System\n");
             MenuOption.ShowOptions();
-            Console.Write("Select an option: ");
+            Console.Write("\nSelect an option: ");
 
             switch (Console.ReadLine())
             {
                 case "1":
-                    Console.WriteLine("You selected Inventory");
+                    Console.WriteLine("\nYou selected Inventory\n");
                     InventoryOption.ShowOptions();
+                    Console.WriteLine("\nSelect an option: ");
+                    switch (Console.ReadLine())
+                    {
+                        case "1":
+                            Console.WriteLine("\nDisplaying Inventory");
+                            break;
+                        case "2":
+                            Console.WriteLine("\nYou Selected Add New Product");
+                            break;
+                        case "3":
+                            Console.WriteLine("\nYou Selected Modify Existing Product");
+                            break;
+                        case "4":
+                            Console.WriteLine("\nYou Selected Remove Product");
+                            break;
+                        case "5":
+                            Console.WriteLine("\nReturning to Previous Menu...");
+                            break;
+                        case "6":
+                            Console.WriteLine("\nExiting...");
+                            exit = true;
+                            break;
+                    }
+
                     break;
                 case "2":
-                    Console.WriteLine("You selected Orders");
+                    Console.WriteLine("\nYou selected Orders\n");
+                    OrderOption.ShowOptions();
+                    Console.WriteLine("\nSelect an option: ");
+                    switch (Console.ReadLine())
+                    {
+                        case "1":
+                            Console.WriteLine("\nYou Selected Customer Orders");
+                            break;
+                        case "2":
+                            Console.WriteLine("\nYou Selected Backorders");
+                            break;
+                        case "3":
+                            Console.WriteLine("\nDisplaying Past Orders");
+                            break;
+                        case "4":
+                            Console.WriteLine("\nReturning to Previous Menu...");
+                            break;
+                        case "5":
+                            Console.WriteLine("\nExiting...");
+                            exit = true;
+                            break;
+                    }
                     break;
                 case "3":
-                    Console.WriteLine("You selected Sales Forecasts");
+                    Console.WriteLine("\nYou selected Sales Forecasts\n");
+                    ForecastOption.ShowOptions();
+                    Console.WriteLine("\nSelect an option: ");
+                    switch (Console.ReadLine())
+                    {
+                        case "1":
+                            Console.WriteLine("\nYou Selected Create Forecast");
+                            break;
+                        case "2":
+                            Console.WriteLine("\nDisplaying Past Forecasts");
+                            break;
+                        case "3":
+                            Console.WriteLine("\nReturning to Previous Menu...");
+                            break;
+                        case "4":
+                            Console.WriteLine("\nExiting...");
+                            exit = true;
+                            break;
+                    }
                     break;
                 case "4":
-                    Console.WriteLine("Exiting...");
+                    Console.WriteLine("\nExiting...");
                     exit = true;
                     break;
                 default:
-                    Console.WriteLine("Invalid option. Please try again.");
+                    Console.WriteLine("\nInvalid option. Please try again.");
                     break;
             }
 
             if (!exit)
             {
-                Console.WriteLine("\nPress any key to return to the menu...");
+                Console.WriteLine("\nPress any key to continue");
                 Console.ReadKey();
             }
         }
