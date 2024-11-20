3643165ae04a1bd4d9b51e590a9eb5fd9cb75a32
OlivierPerron <oli.perron@hotmail.com>
2024-11-20 12:46:39 -0500

    fix

--- FleurCo-API/Classes/SalesForecast.cs
+++ FleurCo-API/Classes/SalesForecast.cs
@@ -3,17 +3,17 @@ namespace FleurCo_API.Classes
     public class SalesForecast
     {
         public DateTime TimeFrame { get; set; }
-        public int ForecastStockLevel { get; set; }
+        public double ForecastStockLevel { get; set; }
         public string ProductCategory { get; set; }
         public List<SalesForecast> ForecastHistory { get; set; }
 
-        public SalesForecast(DateTime timeframe, int forecaststocklevel, string productcategory, List<SalesForecast> forecasthistory)
-        {
-            TimeFrame = timeframe;
-            ForecastStockLevel = forecaststocklevel;
-            ProductCategory = productcategory;
-            ForecastHistory = forecasthistory;
-        }
+        // public SalesForecast(DateTime timeframe, double forecaststocklevel, string productcategory, List<SalesForecast> forecasthistory)
+        // {
+        //     TimeFrame = timeframe;
+        //     ForecastStockLevel = forecaststocklevel;
+        //     ProductCategory = productcategory;
+        //     ForecastHistory = forecasthistory;
+        // }
         public void GetSalesForecast()
         {
 
